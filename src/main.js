c856d2136d5e068f67991e2638fbc6a65bafd868
RichartR <rubiorichart@gmail.com>
2025-10-22 16:15:22 +0200

    Header funcional

--- src/main.js
+++ src/main.js
@@ -1,21 +1,110 @@
-import './style.scss'
+import { renderHeader } from './components/header';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//eslint-disable-next-line
+//import * as bulma from 'bulma';
 
-setupCounter(document.querySelector('#counter'))
+document.addEventListener('DOMContentLoaded', () => {
+  
+  const app = document.querySelector('#app');
+  const header = document.querySelector('#header');
+  const footer = document.querySelector('#footer');
+
+  const URL = "https://xcaoefgporrjzlirttaz.supabase.co/rest/v1/navigation_menu";
+  const KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhjYW9lZmdwb3JyanpsaXJ0dGF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1OTc2NjQsImV4cCI6MjA3NjE3MzY2NH0.Ju9E4JYmn41QrKkmPVaYQq9odhhEDx0JmqtHA_0-NeI";
+
+  /* const data = {
+    "name": "Raton",
+    "price": 15
+  }; */
+  renderHeader().then(data => header.innerHTML = data);
+  
+  //header.innerHTML = renderHeader();
+
+  //obtenerDatos(URL, KEY, header);
+  
+  //updateDatos(URL, KEY, data, 6);
+
+
+
+})
+
+async function obtenerDatos(URL, KEY) {
+    try {
+        const response = await fetch(URL + "?select=*", {
+            method: 'GET',
+            headers: {
+                "apiKey": KEY,
+                "Authorization": "Bearer " + KEY
+            }
+        });
+        
+        const data = await response.json();
+        console.log(data);
+        
+        
+    } catch (error) {
+        console.error('Error al obtener los datos:', error);
+        throw error; // Opcional: re-lanzar el error para que lo maneje quien llame a la función
+    }
+}
+
+function insertDatos(URL, KEY, data){
+  (()=>{
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            "apiKey": KEY,
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + KEY,
+            "Prefer": "return=representation"
+        },
+        body: JSON.stringify(data)
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+    });
+  })();
+}
+
+function updateDatos(URL, KEY, data, id){
+  (()=>{
+    fetch(URL  + `?id=eq.${id}`, {
+        method: 'PATCH',
+        headers: {
+            "apiKey": KEY,
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + KEY,
+            "Prefer": "return=representation"
+        },
+        body: JSON.stringify(data)
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+    });
+  })();
+}
+
+async function uploadImage(projectUrl, KEY, imageBlob, bucket = 'image-test', fileName = 'image.png') {
+  const headersFile = {
+    apiKey: KEY,
+    Authorization: `Bearer ${KEY}`,
+    'x-upsert': 'true'
+  };
+
+  const formData = new FormData();
+  formData.append('file', imageBlob, fileName);
+
+  const response = await fetch(`${projectUrl}/storage/v1/object/${bucket}/${fileName}`, {
+    method: 'POST',
+    headers: headersFile, // do not set Content-Type when sending FormData
+    body: formData
+  });
+
+  if (!response.ok) {
+    throw new Error(`Upload failed: ${response.status} ${response.statusText}`);
+  }
+
+  return await response.json();
+}
