2d767be0289eb136edb3238008e10bddfcdc6fa7
RichartR <rubiorichart@gmail.com>
2025-12-13 20:06:37 +0100

    Modificadas rutas  para vercel

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -10,10 +10,12 @@ class WelcomeComponent extends HTMLElement {
   }
 
   render() {
+    const bannerUrl = new URL('../../images/banner.jpg', import.meta.url).href;
+
     this.innerHTML = `
       <div class="welcome-container">
         <div class="welcome-banner">
-          <img src="/src/images/banner.jpg" alt="NSHRPG Banner" class="banner-image">
+          <img src="${bannerUrl}" alt="NSHRPG Banner" class="banner-image">
           <div class="welcome-overlay">
             <h1 class="welcome-title">Bienvenido a NSHRPG</h1>
             <p class="welcome-subtitle">Explora el mundo ninja y descubre todas las técnicas, aldeas y habilidades</p>
